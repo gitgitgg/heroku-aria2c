7591a3fb51cd06a7b1a56f2f08a8c962fef37522
hkuvidaexk <43096526+hkuvidaexk@users.noreply.github.com>
2020-02-14 07:14:18 +0800

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -4,6 +4,19 @@ filePath=$3
 relativePath=${filepath#./downloads/}
 topPath=./downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
 
+
+find /"$filepath"/. -name *padding* -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.url -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.chm -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.txt -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.png -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.torrent -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.55h.me.mp4 -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.mht -print -exec rm -rf {} \;;
+find /"$filepath"/. -name *.gif -print -exec rm -rf {} \;;
+find /"$filepath"/. -type f -name *.mp4 -size -70M -exec rm {} \;;
+
+
 echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
 if [[ $2 -eq 1 ]]; then # single file
